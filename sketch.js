51e8af636fe2f430366c7dc02716a14a3181b57e
Shaurya443 <81462790+Shaurya443@users.noreply.github.com>
2021-08-02 13:27:20 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,19 +10,21 @@ var bg ;
 
 function preload() {
     // create getBackgroundImg( ) here
+    getBackgroundImg()
 }
 
 function setup(){
     var canvas = createCanvas(1200,700);
     engine = Engine.create();
     world = engine.world;
-
+  
 }
 
 function draw(){
 
     // add condition to check if any background image is there to add
-
+if(backgroundImg)
+background(backgroundImg)
 
     Engine.update(engine);
 
@@ -34,15 +36,37 @@ function draw(){
 async function getBackgroundImg(){
 
     // write code to fetch time from API
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
 
     //change the data in JSON format
+    var responseJSON = await response.json()
 
     // write code slice the datetime
+    var datetime = responseJSON.datetime
+    var hour = datetime.slice(11,13)
+     // add conditions to change the background images from sunrise to sunset
 
-
-    // add conditions to change the background images from sunrise to sunset
-
-
+   if(hour >= 04 && hour <= 06){
+bg = "sunrise1.png"
+   }else if(hour >=06 && hour<=08){
+    bg = "sunrise2.png" 
+   } else if(hour >=23 && hour==0){
+    bg = "sunset10.png" 
+   } else if(hour ==0 && hour<=03){
+    bg = "sunset11.png" 
+   }else{
+       bg ="sunset12.png"
+   }
     //load the image in backgroundImg variable here
-
+   backgroundImg = loadImage(bg)
 }
+
+   
+
+    
+
+   
+
+   
+
+
